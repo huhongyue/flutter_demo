9f7167367ff7c40f41a9778c38913b8f1c9f86fa
yueyue <15858775041@163.com>
2020-01-01 16:31:48 +0800

    'ceshi'

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int _count = 0;
+  int _count2=1;
 
   void _incrementCounter() {
     setState(() {
